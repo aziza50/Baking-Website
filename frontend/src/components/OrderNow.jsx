924412082334c7c297aaabdc16e9b990aee5f85f
aziza50 <aziza.ergasheva50@gmail.com>
2025-08-25 22:14:42 -0400

    further update to menu page

---
+++ frontend/src/components/OrderNow.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Marquee from "react-fast-marquee";
+const OrderNow = () => {
+  return (
+    <div>
+      <Marquee className="font-dawn text-5xl text-[#74070E] overflow-hidden m-10">
+        Order Now * Order Now * Order Now * Order Now * Order Now * Order Now
+      </Marquee>
+    </div>
+  );
+};
+
+export default OrderNow;
